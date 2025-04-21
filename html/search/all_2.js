69e868403128d9827cd5067e7f01d8b3e5bb0c9e
Justin Haddock <jhaddoc5@ut.utm.edu>
2025-04-21 17:37:58 -0500

    Fixed doxygen config

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['supervisor_2',['Supervisor',['../classSupervisor.html',1,'']]]
+  ['employee_2',['Employee',['../classEmployee.html',1,'']]],
+  ['employee_2ecpp_3',['Employee.cpp',['../Employee_8cpp.html',1,'']]],
+  ['employee_2eh_4',['Employee.h',['../Employee_8h.html',1,'']]]
 ];
